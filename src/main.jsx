b091cae7318ed482c3ae894f089f43992f629e23
Nwaobiala <uche@expd.co.uk>
2026-07-11 22:54:35 +0100

    r.u.i

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App.jsx'
 import './index.css'
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
