7c90a0074958784fbdd6a9b5310d5c9b379e233f
Nwaobiala <uche@expd.co.uk>
2026-08-31 16:33:51 +0100

    adjust h to be resposive in dailog

--- src/components/ui/AlertDialog.tsx
+++ src/components/ui/AlertDialog.tsx
@@ -45,7 +45,7 @@ export default function AlertDialog({ trigger, title, description, children, typ
                 <AlertDialogPrimitive.Overlay className=" fixed inset-0 z-[50] bg-black/40 backdrop-blur-sm "/>
 
                 <AlertDialogPrimitive.Content
-                    className= {`fixed left-1/2 top-1/2 z-[60] flex w-[80%] md:w-[60%] lg:w-[40%] -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-2xl bg-white shadow-xl`}>
+                    className= {`fixed left-1/2 top-1/2 z-[60] flex w-[90vw] md:w-[80vw] min-h-0 max-h-[90vh] -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-2xl bg-white shadow-xl`}>
 
                     {/* HEADER */}
                     <div className="flex items-center justify-between px-6 py-4">
@@ -112,8 +112,6 @@ export default function AlertDialog({ trigger, title, description, children, typ
                                 }`}>
                                 {actionText}
                             </AlertDialogPrimitive.Action>
-                            
-
                         </div>
                     )}
 
